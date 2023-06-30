a380f1d5b5f7dde7079c5e582a2b4de9a040b656
Daniel Scharf <id.scharfd@gmail.com>
2023-06-30 09:56:56 -0700

    added a bunch of js for the backround logic. still not done

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function calculateTip(params) {
 }
 
 function tipCalc(params) {
-
+  const percentage = 0;
+  if (tip.value === '5%') {
+    percentage = .05;
+  } else if (tip.value === '10%'){
+    percentage = .1;
+  } else if (tip.value === '15%') {
+    percentage = .15;
+  } else if (tip.value === '20%') {
+    percentage = .2;
+  } else if (tip.value === '25%') {
+    percentage = .25;
+  } else {
+    percentage = tip.value / 10
+  }
+  return percentage
 }
